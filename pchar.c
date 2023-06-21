c109c377f281b8de59968fd1da620a9e885c4a2b
khawla <boukniter.khawla@gmail.com>
2023-06-21 10:57:41 -0700

    all

---
+++ pchar.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * pchar - prints int at the top of the stack as char
+ * @s: stack given by main
+ * @cnt: amount of line
+ */
+void pchar(stack_t **s, unsigned int cnt)
+{
+	if (!s || !(*s))
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", cnt);
+		exit(EXIT_FAILURE);
+	}
+	if (isascii((*s)->n) == 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", cnt);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", (*s)->n);
+}
+
