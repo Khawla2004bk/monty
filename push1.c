e6d819976e07e64634e0c02cb42a470ef55914b4
mohamed hossam <Baizmohamedhossam@gmail.com>
2023-06-21 12:51:27 -0700

    all

---
+++ push1.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * push - push element into the stack
+ * @s: stack given by main
+ * @cnt: amount of lines
+ */
+
+void push(stack_t **s, unsigned int cnt)
+{
+	char *i = global.arg;
+
+	if (i == NULL || *i == '\0')
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", cnt);
+		exit(EXIT_FAILURE);
+	}
+
+	for (; *i != '\0'; i++)
+	{
+		if (!isdigit(*i) && *i != '-')
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", cnt);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (global.data == 1)
+	{
+		if (!addnode(s, atoi(global.arg)))
+			exit(EXIT_FAILURE);
+	}
+	else
+	{
+		if (!queue(s, atoi(global.arg)))
+			exit(EXIT_FAILURE);
+	}
+}
